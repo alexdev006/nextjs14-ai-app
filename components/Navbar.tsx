8282c0b47f2cdf000a4ab737dde6d878698e9ee8
Alex <alexdev0605@gmail.com>
2024-03-23 13:55:47 +0100

    feat: init

---
+++ components/Navbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { NavbarDrawer } from "./NavbarDrawer";
+import { Navlink } from "@/types/NavLinks";
+import { ToggleDarkModeButton } from "./ToggleDarkModeButton";
+
+const navLinks: Navlink[] = [
+  { label: "Accueil", href: "/" },
+  { label: "About us", href: "/about" },
+  { label: "Contact", href: "/contact" },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+  return (
+    <div className="border border-b-2 p-6 flex items-center justify-between ">
+      <div className="block sm:hidden">
+        <NavbarDrawer navLinks={navLinks} />
+      </div>
+      <div className="">Logo</div>
+      <div className="hidden sm:block">
+        <nav>
+          {navLinks.map(({ label, href }: Navlink) => (
+            <Link
+              href={href}
+              key={label}
+              className={`mx-2 text-xl ${
+                pathname === href
+                  ? "font-bold underline underline-offset-8 decoration-indigo-500 decoration-4"
+                  : ""
+              }`}
+            >
+              {label}
+            </Link>
+          ))}
+        </nav>
+      </div>
+      <div className="">
+        <ToggleDarkModeButton />
+      </div>
+    </div>
+  );
+}
---
+++ components/NavbarDrawer.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import * as React from "react";
+import { Menu, X } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Navlink } from "@/types/NavLinks";
+
+interface NavbarDrawerProps {
+  navLinks: Navlink[];
+}
+
+export function NavbarDrawer({ navLinks }: NavbarDrawerProps) {
+  const pathname = usePathname();
+
+  return (
+    <Drawer direction="left">
+      <DrawerTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Menu />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent className="h-full w-3/4 flex flex-col">
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerClose asChild>
+              <Button variant="outline" size="icon">
+                <X />
+              </Button>
+            </DrawerClose>
+          </DrawerHeader>
+          <div className="p-4 pb-0">
+            <nav className="flex flex-col">
+              {navLinks.map(({ label, href }: Navlink) => (
+                <DrawerTrigger asChild key={label}>
+                  <Link
+                    href={href}
+                    className={`my-2 text-xl ${
+                      pathname === href
+                        ? "font-bold underline underline-offset-8 decoration-indigo-500 decoration-4 "
+                        : ""
+                    }`}
+                  >
+                    {label}
+                  </Link>
+                </DrawerTrigger>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
